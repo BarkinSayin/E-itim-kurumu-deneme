21e9aada7329e0d8cd341e8426aadf62d1995620
BarkinSayin <barkinsayin@hotmail.com>
2024-03-11 16:44:53 +0300

    create Person class

---
+++ src.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(firstName, lastName, age, adress, phone) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.adress = adress;
+    this.phone = phone;
+  }
+}
+
+
